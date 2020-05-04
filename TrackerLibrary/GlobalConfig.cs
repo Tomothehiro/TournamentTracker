1d4e0628d4c6fdba6769ff84dd2d2f0977546158
Tomothehiro <tomonouchi@hotmail.com>
2020-05-04 16:25:47 -0700

    SQL connection

--- TrackerLibrary/GlobalConfig.cs
+++ TrackerLibrary/GlobalConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,23 +10,35 @@ namespace TrackerLibrary
 {
     public static class GlobalConfig
     {
-        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
+        public static IDataConnection Connection { get; private set; }
 
-        public static void InitializeConnections(bool database, bool textFiles)
+        public static void InitializeConnections(DatabaseType db)
         {
-            if (database)
+            switch (db)
             {
-                // TODO - Create the SQL Connectotr properly
-                SqlConnector sql = new SqlConnector();
-                Connections.Add(sql);
+                case DatabaseType.Sql:
+                    // TODO - Create the SQL Connectotr properly
+                    SqlConnector sql = new SqlConnector();
+                    Connection = sql;
+                    break;
+                case DatabaseType.TextFile:
+                    // TODO - Create the Text Connection
+                    TextConnector text = new TextConnector();
+                    Connection = text;
+                    break;
+                default:
+                    break;
             }
+        }
 
-            if (textFiles)
-            {
-                // TODO - Create the Text Connection
-                TextConnector text = new TextConnector();
-                Connections.Add(text);
-            }
+        /// <summary>
+        /// Returns the connectionString from App.config by querying for its name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static string ConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
         }
     }
 }
